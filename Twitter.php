273369b21503aea9b557f2136b824ef3473a6ab2
rangsimant <hanwongsa.r@gmail.com>
2016-04-18 18:28:50 +0700

    remove endcode utf8

--- Twitter.php
+++ Twitter.php
@@ -1027,7 +1027,6 @@ class Twitter
     {
         // build parameters
         $parameters['status'] = (string) $status;
-        $parameters['status'] = utf8_encode($parameters['status']);
         if ($inReplyToStatusId != null) {
             $parameters['in_reply_to_status_id'] = (string) $inReplyToStatusId;
         }
